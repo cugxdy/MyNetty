f1c504456dbc39a8a25432b340ebc5d756fd98e7
cugxdy <cugxdy@163.com>
2019-08-10 15:07:43 +0800

    内存分配的接口类对象,定义了创建ByteBuf对象的基本动作

--- io/netty/buffer/ByteBufAllocator.java
+++ io/netty/buffer/ByteBufAllocator.java
@@ -21,18 +21,21 @@ package io.netty.buffer;
  */
 public interface ByteBufAllocator {
 
+	// 默认内存分配对象
     ByteBufAllocator DEFAULT = ByteBufUtil.DEFAULT_ALLOCATOR;
 
     /**
      * Allocate a {@link ByteBuf}. If it is a direct or heap buffer
      * depends on the actual implementation.
      */
+    // 创建ByteBuf对象,它是堆外内存还是堆内存依据具体的实现类
     ByteBuf buffer();
 
     /**
      * Allocate a {@link ByteBuf} with the given initial capacity.
      * If it is a direct or heap buffer depends on the actual implementation.
      */
+    // 创建指定容量大小的ByteBuf对象,它是堆外内存还是堆内存依据具体的实现类
     ByteBuf buffer(int initialCapacity);
 
     /**
@@ -40,89 +43,106 @@ public interface ByteBufAllocator {
      * maximal capacity. If it is a direct or heap buffer depends on the actual
      * implementation.
      */
+    // 创建指定容量大小和最大容量的ByteBuf对象,它是堆外内存还是堆内存依据具体的实现类
     ByteBuf buffer(int initialCapacity, int maxCapacity);
 
     /**
      * Allocate a {@link ByteBuf}, preferably a direct buffer which is suitable for I/O.
      */
+    // 创建ByteBuf对象,它是堆外内存实现
     ByteBuf ioBuffer();
 
     /**
      * Allocate a {@link ByteBuf}, preferably a direct buffer which is suitable for I/O.
      */
+    // 创建指定容量大小的ByteBuf对象,它是堆外内存实现
     ByteBuf ioBuffer(int initialCapacity);
 
     /**
      * Allocate a {@link ByteBuf}, preferably a direct buffer which is suitable for I/O.
      */
+    // 创建指定容量大小和最大容量的ByteBuf对象,它是堆外内存实现
     ByteBuf ioBuffer(int initialCapacity, int maxCapacity);
 
     /**
      * Allocate a heap {@link ByteBuf}.
      */
+    // 创建ByteBuf对象,它是堆内存实现
     ByteBuf heapBuffer();
 
     /**
      * Allocate a heap {@link ByteBuf} with the given initial capacity.
      */
+    // 创建指定容量大小的ByteBuf对象,它是堆内存实现
     ByteBuf heapBuffer(int initialCapacity);
 
     /**
      * Allocate a heap {@link ByteBuf} with the given initial capacity and the given
      * maximal capacity.
      */
+    // 创建指定容量大小和最大容量的ByteBuf对象,它是堆内存实现
     ByteBuf heapBuffer(int initialCapacity, int maxCapacity);
 
     /**
      * Allocate a direct {@link ByteBuf}.
      */
+    // 创建ByteBuf对象,它是堆外内存实现
     ByteBuf directBuffer();
 
     /**
      * Allocate a direct {@link ByteBuf} with the given initial capacity.
      */
+    // 创建指定容量大小的ByteBuf对象,它是堆外内存实现
     ByteBuf directBuffer(int initialCapacity);
 
     /**
      * Allocate a direct {@link ByteBuf} with the given initial capacity and the given
      * maximal capacity.
      */
+    // 创建指定容量大小和最大容量的ByteBuf对象,它是堆外内存实现
     ByteBuf directBuffer(int initialCapacity, int maxCapacity);
 
     /**
      * Allocate a {@link CompositeByteBuf}.
      * If it is a direct or heap buffer depends on the actual implementation.
      */
+    // 创建CompositeByteBuf对象,它是堆外内存还是堆内存依据具体的实现类
     CompositeByteBuf compositeBuffer();
 
     /**
      * Allocate a {@link CompositeByteBuf} with the given maximum number of components that can be stored in it.
      * If it is a direct or heap buffer depends on the actual implementation.
      */
+    // 创建指定容量大小的CompositeByteBuf对象,它是堆外内存还是堆内存依据具体的实现类
     CompositeByteBuf compositeBuffer(int maxNumComponents);
 
     /**
      * Allocate a heap {@link CompositeByteBuf}.
      */
+    // 创建CompositeByteBuf对象,它是堆内存实现
     CompositeByteBuf compositeHeapBuffer();
 
     /**
      * Allocate a heap {@link CompositeByteBuf} with the given maximum number of components that can be stored in it.
      */
+    // 创建指定容量大小的CompositeByteBuf对象,它是堆内存实现
     CompositeByteBuf compositeHeapBuffer(int maxNumComponents);
 
     /**
      * Allocate a direct {@link CompositeByteBuf}.
      */
+    // 创建CompositeByteBuf对象,它是堆外内存实现
     CompositeByteBuf compositeDirectBuffer();
 
     /**
      * Allocate a direct {@link CompositeByteBuf} with the given maximum number of components that can be stored in it.
      */
+    // 创建指定容量大小的CompositeByteBuf对象,它是堆外内存实现
     CompositeByteBuf compositeDirectBuffer(int maxNumComponents);
 
     /**
      * Returns {@code true} if direct {@link ByteBuf}'s are pooled
      */
+    // 判断基于堆外内存的ByteBuf对象是否为池化
     boolean isDirectBufferPooled();
 }
